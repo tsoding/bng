34cb276d45a2337286f6bcc8fa33137489657349
rexim <reximkut@gmail.com>
2020-09-11 17:47:50 +0700

    Introduce pixel format to the BNG format

--- bng.h
+++ bng.h
@@ -3,12 +3,22 @@
 
 #define BNG_MAGIC 0x21474E42
 
+#if defined(__GNUC__) || defined(__clang__)
+#  define PACKED __attribute__((packed))
+#else
+#  warning "Packed attributes for struct is not implemented for this compiler. Feel free to fix that and submit a Pull Request to https://github.com/tsoding/bng"
+#endif
+
 struct Bng
 {
     uint32_t magic;
     uint32_t width;
     uint32_t height;
+    uint8_t red_byte;           // 0
+    uint8_t green_byte;         // 1
+    uint8_t blue_byte;          // 2
+    uint8_t alpha_byte;         // 3
     uint8_t data[];
-};
+} PACKED;
 
 #endif  // BNG_H_
--- png2bng.c
+++ png2bng.c
@@ -2,18 +2,31 @@
 #include "./stb_image.h"
 #include "./bng.h"
 
-int main()
+int main(int argc, char **argv)
 {
+    if (argc < 3) {
+        fprintf(stderr, "png2bng <input.png> <output.bng>\n");
+        exit(1);
+    }
+
+    const char *input_filepath = argv[1];
+    const char *output_filepath = argv[2];
+
     int x, y, n;
-    unsigned char *data = stbi_load("./tsodinw.png", &x, &y, &n, 4);
+    unsigned char *data = stbi_load(input_filepath, &x, &y, &n, 4);
     assert(data != NULL);
 
-    struct Bng bng = {};
-    bng.magic = BNG_MAGIC;
-    bng.width = x;
-    bng.height = y;
+    struct Bng bng = {
+        .magic      = BNG_MAGIC,
+        .width      = x,
+        .height     = y,
+        .red_byte   = 0,
+        .green_byte = 1,
+        .blue_byte  = 2,
+        .alpha_byte = 3,
+    };
 
-    FILE *out = fopen("./tsodinw.bng", "wb");
+    FILE *out = fopen(output_filepath, "wb");
     fwrite(&bng, 1, sizeof(bng), out);
     fwrite(data, 1, x * y * 4, out);
     fclose(out);
